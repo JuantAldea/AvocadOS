ca56346a4f543a0a1e9a29d20a67f0fbf1795c39
Juan Antonio <JuantAldea@users.noreply.github.com>
2021-01-29 21:22:19 +0100

    Read from disk using CHS addressing

--- boot.asm
+++ boot.asm
@@ -19,19 +19,6 @@ init_code_segment:
     ; A good practice is to enforce CS:IP at the very start of your boot sector.
     jmp 0x7c0:start
 
-handle_zero:
-    mov al, 'A'
-    mov ah, 0eh
-    mov bx, 0x0
-    int 0x10
-    iret
-
-handle_one:
-    mov al, 'B'
-    mov ah, 0eh
-    mov bx, 0x0
-    int 0x10
-    iret
 start:
 
     cli ; int off TODO: should this be performed here or before?
@@ -42,26 +29,26 @@ start:
     mov ax, 0x00
     mov ss, ax ; stack segment
     mov sp, 0x7c00 ; stack pointer
-
-    ; set IVT [[offset,segment]_0,[offset, segment]_1, ...] 
-    ; IVT is used in real mode, whereas IDT is used in protected mode
-    ; int 0 is div-by-zero exception
-    mov word[ss:0x00], handle_zero ; make mov use SS segment instead of DS
-                                   ; the interrupt vector table starts at 
-                                   ; byte 0 of segment
-    mov word[ss:0x02], 0x7c0
-    
-    mov word[ss:0x04], handle_one
-    mov word[ss:0x06], 0x7c0
-
     sti ; int on
 
-    int 1
-
-    mov si, message
-    mov byte al, [es:32]
+    ; int 0x13/ah=0x2-> read data from disk using CHS addressing
+    mov ah, 2 ; read sector command
+    mov al, 1 ; one sector
+    mov ch, 0 ; cylinder low 8 bits
+    mov cl, 2 ; read second sector, as the message is loaded from byte 512. For CHS addressing, sectors start at 1
+    mov dh, 0 ; head number
+    ; mov dl, XXX ; there's no need to set the Drive Number as the bios sets it for us to the one we are booting from
+    mov bx, buffer ; ES register (extra segment) already points to 0x7c0
+    int 0x13 ;invoke ISR 13
+    jc error
+    mov si, buffer
     call print
-    jmp $ ;jump to itself
+    jmp $ ; trap
+
+error:
+    mov si, error_message
+    call print
+    jmp $ ;trap
 
 print:
     mov bx, 0
@@ -81,8 +68,11 @@ print_char:
     ret
 
 
-message:db 'Hello World!', 0 ; message + \0
+error_message:db 'Failed to load sector', 0 ; message + \0
 
 times 510 - ($ - $$) db 0 ; fill 510 bytes as 0 (510 - (current_addr - begin)))
 
-dw 0xAA55; (little endian, 0x055AA is the boot signatura)
+dw 0xAA55; (little endian, 0x055AA is the boot signature)
+
+; BIOS wont load anthing from here, as it loads only 512 bytes
+buffer: ; nothing here, just a buffer
