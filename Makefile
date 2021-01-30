56a9221ca6330400dd631277eba9f017a496fe43
Juan Antonio <JuantAldea@users.noreply.github.com>
2021-01-30 10:41:01 +0100

    Entering 32 bits protected mode

--- Makefile
+++ Makefile
@@ -1,12 +1,10 @@
 all: boot.bin
 
-boot.bin: boot.asm message.txt
-	nasm -f bin ./boot.asm -o boot.bin
-	dd if=message.txt >> boot.bin
-	# to complete the second 512 byte sector (+ some spare zeroes)
-	dd if=/dev/zero bs=512 count=1 >> boot.bin
+boot.bin: src/boot/boot.asm
+	nasm -f bin src/boot/boot.asm -o bin/boot.bin
+
 run: boot.bin
-	qemu-system-x86_64 -hda boot.bin
+	qemu-system-x86_64 -hda bin/boot.bin
 
 clean:
-	rm boot.bin
+	rm -rf bin/boot.bin
--- boot.asm
+++ boot.asm
@@ -1,6 +1,10 @@
 ; http://www.ctyme.com/rbrown.htm
-ORG 0
-BITS 16 ;16 bit code (for the assembler)
+ORG 0x7c00
+BITS 16 ; 16 bit code (for the assembler)
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 ; account for the BIOS Parameter block BPB
 ; https://wiki.osdev.org/FAT#BPB_.28BIOS_Parameter_Block.29
 _start:
@@ -8,6 +12,36 @@ _start:
     nop
 ; filler, as the BIOS might write the BPB into this section.
 times 33 db 0
+
+; GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+; offset 0x8
+gdt_code:       ; CS should point to this
+    dw 0xffff   ; segment limit 0-15
+    dw 0x0      ; base 0-15 bits
+    db 0x0      ; base 16-23
+    db 0x9a     ; access byte
+    db 11001111b ; high & low 4 bit  fags
+    db 0x0       ; base 24-31
+
+;offset 0x10
+gdt_data:       ; ds, ss, es, fs, gs
+    dw 0xffff   ; segment limit 0-15
+    dw 0x0      ; base 0-15 bits
+    db 0x0      ; base 16-23
+    db 0x92     ; access byte
+    db 11001111b ; high & low 4 bit  fags
+    db 0x0       ; base 24-31
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ; length of the descriptor
+    dd gdt_start    ; address of the descriptor
 
 init_code_segment:
     ; The (legacy) BIOS checks bootable devices for a boot signature, a so called magic number
@@ -17,62 +51,55 @@ init_code_segment:
     ; However, some BIOS' load to 0x7c0:0x0000 (segment 0x07c0, offset 0), which resolves to the
     ; same physical address, but can be surprising. 
     ; A good practice is to enforce CS:IP at the very start of your boot sector.
-    jmp 0x7c0:start
+    jmp 0:start ; now the code is located at 0x7c00 so segment is 0x7c00
 
 start:
-
     cli ; int off TODO: should this be performed here or before?
-    mov ax, 0x7c0
-    
+    mov ax, 0x00 ; segment offset is 0
     mov ds, ax ; data segment
     mov es, ax ; special segment
-    mov ax, 0x00
     mov ss, ax ; stack segment
     mov sp, 0x7c00 ; stack pointer
     sti ; int on
 
-    ; int 0x13/ah=0x2-> read data from disk using CHS addressing
-    mov ah, 2 ; read sector command
-    mov al, 1 ; one sector
-    mov ch, 0 ; cylinder low 8 bits
-    mov cl, 2 ; read second sector, as the message is loaded from byte 512. For CHS addressing, sectors start at 1
-    mov dh, 0 ; head number
-    ; mov dl, XXX ; there's no need to set the Drive Number as the bios sets it for us to the one we are booting from
-    mov bx, buffer ; ES register (extra segment) already points to 0x7c0
-    int 0x13 ;invoke ISR 13
-    jc error
-    mov si, buffer
-    call print
-    jmp $ ; trap
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-error:
-    mov si, error_message
-    call print
-    jmp $ ;trap
+[BITS 32]
+load32:
+    ; now the BIOS is out of reach
+    ; set data registers
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    ; set stack pointer
+    mov ebp, 0x00200000
+    mov esp, ebp
 
-print:
-    mov bx, 0
-.loop:
-    lodsb ; load *(ds:si) into al, then increment si 
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
+    ; enable A20 line (21th bit)
+    ; assumming fast A20 latch
+    ; However, the Fast A20 method is not supported everywhere and there is no reliable way
+    ; to tell if it will have some effect or not on a given system. Even worse, on some systems,
+    ; it may actually do something else like blanking the screen, so it should be used only after
+    ; the BIOS has reported that FAST A20 is available. Code for systems lacking FAST A20 support
+    ; is also needed, so relying only on this method is discouraged. Also, on some chipsets you
+    ; might have to enable Fast A20 support in the BIOS configuration screen.
+    in al, 0x92
+    or al, 2
+    out 0x92, al
 
-
-print_char:
-    mov ah, 0eh
-    int 0x10 ;print (bios)
-    ret
-
-
-error_message:db 'Failed to load sector', 0 ; message + \0
+    jmp $
 
 times 510 - ($ - $$) db 0 ; fill 510 bytes as 0 (510 - (current_addr - begin)))
 
 dw 0xAA55; (little endian, 0x055AA is the boot signature)
 
 ; BIOS wont load anthing from here, as it loads only 512 bytes
-buffer: ; nothing here, just a buffer
