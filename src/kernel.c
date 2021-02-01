559d5afe2b8146d11d9c8aaed22bf88104e1327a
Juan Antonio <JuantAldea@users.noreply.github.com>
2021-02-01 14:05:37 +0100

    Add splash screen function

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,20 @@
 #include "kernel.h"
 #include "termio.h"
 
+void kernel_splash()
+{
+    print("\n\n\n\n\n");
+    terminal_put_str("                  _                                _    ___  __\n", 2);
+    terminal_put_str("                 /_\\ __   __ ___    ___  __ _   __| |  /___\\/ _\\\n", 2);
+    terminal_put_str("                //_\\\\\\ \\ / // _ \\  / __|/ _` | / _` | //  //\\ \\\n", 2);
+    terminal_put_str("               /  _  \\\\ V /| (_) || (__| (_| || (_| |/ \\_// _\\ \\\n", 2);
+    terminal_put_str("               \\_/ \\_/ \\_/  \\___/  \\___|\\__,_| \\__,_|\\___/  \\__/\n", 2);
+}
+
 void kernel_main(void)
 {
     terminal_initialize();
-
-    print("Would you fancy an avocado?");
+    kernel_splash();
 
 trap:
     goto trap;
