81d185f1fcf0fc84d5394b30c068d8b4fca507d2
Juan Antonio <JuantAldea@users.noreply.github.com>
2021-02-05 23:28:18 +0100

    Add intb/w outb/w functions

---
+++ src/io/io.asm
@@ -0,0 +1,58 @@
+section .asm
+
+global insb
+global insw
+global outb
+global outw
+
+insb:
+    ;create stack frame
+    push ebp
+    mov ebp, esp
+    
+    xor eax, eax
+    mov edx, [ebp + 8]
+    in al, dx
+
+    pop ebp
+    ret
+
+insw:
+    push ebp
+    mov ebp, esp
+    xor eax, eax
+    mov edx, [ebp + 8]
+    in ax, dx
+    
+    pop ebp
+    ret
+
+outb:
+    push ebp
+    mov ebp, esp
+    ;push eax
+    ;push edx
+   
+    mov eax, [ebp + 12]
+    mov edx, [ebp + 8]
+    out dx, al
+    
+    ;pop edx
+    ;pop eax
+    pop ebp
+    ret
+
+outw:
+    push ebp
+    mov ebp, esp
+    ;push eax
+    ;push edx
+    mov eax, [ebp + 12]
+    mov edx, [ebp + 8]
+    
+    out dx, ax
+    
+    ;pop edx
+    ;pop eax
+    pop ebp
+    ret
---
+++ src/io/io.h
@@ -0,0 +1,15 @@
+#ifndef __IO_H
+#define __IO_H
+
+#include <stdint.h>
+
+// https://man7.org/linux/man-pages/man2/insb.2.html
+// why ushort instead of uint16_t?
+
+uint8_t insb(uint16_t port);
+uint16_t insw(uint16_t port);
+
+void outb(uint16_t port, uint8_t value);
+void outw(uint16_t port, uint16_t value);
+
+#endif
