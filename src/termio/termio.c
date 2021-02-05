091534800eeae8c4dc4083ecf9856e7e12989093
Juan Antonio <JuantAldea@users.noreply.github.com>
2021-02-05 23:16:41 +0100

    Add wrapping to termio functions

--- src/termio/termio.c
+++ src/termio/termio.c
@@ -1,7 +1,6 @@
-
 #include "termio.h"
 
-static uint16_t *video_memory = 0;
+static uint16_t *video_memory = (uint16_t *) (0xb8000);
 static uint16_t terminal_row = 0;
 static uint16_t terminal_column = 0;
 
@@ -10,9 +9,8 @@ uint16_t terminal_make_char(const char c, const char colour)
     return (colour << 8) | c;
 }
 
-void terminal_initialize ()
+void terminal_init ()
 {
-    video_memory = (uint16_t *) (0xb8000);
     terminal_row = 0;
     terminal_column = 0;
     uint16_t filler_char = terminal_make_char(' ', 0);
@@ -23,7 +21,7 @@ void terminal_initialize ()
     }
 }
 
-size_t strlen(const char  * str)
+size_t strlen(const char  * const str)
 {
     size_t len = 0;
     while (str[len]) {
@@ -35,13 +33,13 @@ size_t strlen(const char  * str)
 
 void terminal_print_char_x_y(const uint16_t character, const uint16_t x, const uint16_t y)
 {
-    video_memory[y * VGA_WIDTH + x] = character;
+    video_memory[(y * VGA_WIDTH + x) % (VGA_HEIGHT * VGA_WIDTH)] = character;
 }
 
 void terminal_put_char(const uint8_t character, uint8_t colour)
 {
     if (character == '\n') {
-        ++terminal_row;
+        terminal_row = (terminal_row + 1) % VGA_HEIGHT;
         terminal_column = 0;
         return;
     }
@@ -51,15 +49,16 @@ void terminal_put_char(const uint8_t character, uint8_t colour)
     if (terminal_column == VGA_WIDTH)
     {
         terminal_column = 0;
-        ++terminal_row;
+        terminal_row = (terminal_row + 1) % VGA_HEIGHT;
     }
 }
 
 void terminal_print_string_x_y(const char *str, const char colour, const uint8_t x, const uint8_t y)
 {
-    volatile uint16_t *video_ptr = &video_memory[y * VGA_WIDTH + x];
+    size_t index = y * VGA_WIDTH + x;
     while(*str) {
-        *video_ptr++ = terminal_make_char(*str++, colour);
+        video_memory[index++] = terminal_make_char(*str++, colour);
+        index %= (VGA_WIDTH * VGA_HEIGHT);
     }
 }
 
@@ -70,7 +69,7 @@ void terminal_put_str(const char *str, const char colour)
     }
 }
 
-void print(const char *str)
+void print(const char * const str)
 {
     terminal_put_str(str, 15);
 }
--- src/termio/termio.h
+++ src/termio/termio.h
@@ -11,11 +11,11 @@
 void terminal_put_char(const uint8_t character, uint8_t colour);
 uint16_t terminal_make_char(const char c, const char colour);
 
-size_t strlen(const char  * str);
+size_t strlen(const char  * const str);
 void terminal_print_char_x_y(const uint16_t character, const uint16_t x, const uint16_t y);
 void terminal_print_string_x_y(const char *str, const char colour, const uint8_t x, const uint8_t y);
 void terminal_put_str(const char *str, const char colour);
-void terminal_initialize ();
-void print(const char *str);
+void terminal_init();
+void print(const char * const str);
 
 #endif /* __TERMIO_H*/
