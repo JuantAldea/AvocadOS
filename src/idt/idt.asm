3e515121dd72abb658238419a3bdeaa8bdf0944a
Juan Antonio <JuantAldea@users.noreply.github.com>
2021-02-12 09:42:13 +0100

    Fix ASM indentation

--- src/idt/idt.asm
+++ src/idt/idt.asm
@@ -16,52 +16,52 @@ idt_load:
     ret
 
 %macro GENERATE_ISR_ROUTINES 1  ; macro, one parameter
-  GENERATE_RAISE_INT %1
-  ISR %1
+    GENERATE_RAISE_INT %1
+    ISR %1
 %endmacro
 
 %macro GENERATE_RAISE_INT 1  ; macro, one parameter
-  [GLOBAL raise_int_%1]
-  raise_int_%1:
-    int %1
-    ret
+    [GLOBAL raise_int_%1]
+    raise_int_%1:
+        int %1
+        ret
 %endmacro
 
 %macro ISR 1  ; macro, one parameter
-  [GLOBAL isr_%1]
-  isr_%1:
-    cli
-    push byte 0
-    push byte %1
-    jmp isr_wrapper
+    [GLOBAL isr_%1]
+    isr_%1:
+        cli
+        push byte 0
+        push byte %1
+        jmp isr_wrapper
 %endmacro
 
 extern isr_dispatcher
 
 isr_wrapper:
-   pushad                    ; Pushes edi,esi,ebp,esp,ebx,edx,ecx,eax
+    pushad                    ; Pushes edi,esi,ebp,esp,ebx,edx,ecx,eax
 
-   mov ax, ds               ; Lower 16-bits of eax = ds.
-   push eax                 ; save the data segment descriptor
+    mov ax, ds               ; Lower 16-bits of eax = ds.
+    push eax                 ; save the data segment descriptor
 
-   mov ax, 0x10  ; load the kernel data segment descriptor
-   mov ds, ax
-   mov es, ax
-   mov fs, ax
-   mov gs, ax
+    mov ax, 0x10  ; load the kernel data segment descriptor
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
 
-   call isr_dispatcher
+    call isr_dispatcher
 
-   pop eax        ; reload the original data segment descriptor
-   mov ds, ax
-   mov es, ax
-   mov fs, ax
-   mov gs, ax
+    pop eax        ; reload the original data segment descriptor
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
 
-   popad  ; Pops edi,esi,ebp...
-   add esp, 8     ; Cleans up the pushed error code and pushed ISR number
-   sti
-   iret           ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP
+    popad  ; Pops edi,esi,ebp...
+    add esp, 8     ; Cleans up the pushed error code and pushed ISR number
+    sti
+    iret           ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP
 
 GENERATE_ISR_ROUTINES 0x0
 GENERATE_ISR_ROUTINES 0x1
@@ -74,19 +74,19 @@ GENERATE_ISR_ROUTINES 0x21
 [GLOBAL no_interrupt]
 [EXTERN no_int_handler]
 no_interrupt:
-  cli
-  pushad
-  call no_int_handler
-  popad
-  sti
-  iret
+    cli
+    pushad
+    call no_int_handler
+    popad
+    sti
+    iret
 
 [GLOBAL enable_interrupts]
 enable_interrupts:
-  sti
-  ret
+    sti
+    ret
 
 [GLOBAL disable_interrupts]
 disable_interrupts:
-  cli
-  ret
+    cli
+    ret
--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -7,36 +7,36 @@ CODE_SEG equ 0x08
 DATA_SEG equ 0x10
 
 _start_kernel:
-  mov ax, DATA_SEG
-  mov ds, ax
-  mov es, ax
-  mov fs, ax
-  mov gs, ax
-  mov ss, ax
-  ; set stack pointer
-  mov ebp, 0x00200000
-  mov esp, ebp
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    ; set stack pointer
+    mov ebp, 0x00200000
+    mov esp, ebp
 
-  call remap_master_pic
+    call remap_master_pic
 
-  call kernel_main;
+    call kernel_main;
 
-  jmp $ ;trap
+    jmp $ ;trap
 
 ; Programmable Interrupt Controller
 remap_master_pic:
-  push ebp
-  mov ebp, esp
-  mov al, 00010001b
-  out 0x20, al
+    push ebp
+    mov ebp, esp
+    mov al, 00010001b
+    out 0x20, al
 
-  mov al, 0x20 ; int 0x20
-  out 0x21, al
+    mov al, 0x20 ; int 0x20
+    out 0x21, al
 
-  mov al, 00000001b
-  out 0x21, al
-  pop ebp
-  ret
+    mov al, 00000001b
+    out 0x21, al
+    pop ebp
+    ret
 
 ; C compiler requires functions to be aligned to 16 bytes
 ; To ensure this assmebly does not destroy their alignment, as this piece of code
