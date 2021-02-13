66601c5ba233b2d16783885f6fc0386e2d17f63e
Juan Antonio <JuantAldea@users.noreply.github.com>
2021-02-13 23:10:03 +0100

    Implement strcmp & strncmp

--- src/string/string.c
+++ src/string/string.c
@@ -17,6 +17,33 @@ size_t strnlen(const char  * const str, size_t max_len)
     return len;
 }
 
+
+int strcmp(const char *s1, const char *s2)
+{
+    while(*s1 && *s2) {
+        if (*s1 != *s2) {
+            break;
+        }
+        ++s1;
+        ++s2;
+    }
+    return *s1 - *s2;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+    size_t i = 0;
+    while(*s1 && *s2 && i < n) {
+        if (*s1 != *s2) {
+            break;
+        }
+        ++s1;
+        ++s2;
+        ++i;
+    }
+    return *s1 - *s2;
+}
+
 bool is_digit(const char c)
 {
     return c >= '0' && c <= '9';
@@ -29,7 +56,7 @@ int numeric_char_to_digit(const char c)
 
 char digit_to_char(const int c)
 {
-    if (c < 0 || c > 9){
+    if (c < 0 || c > 9) {
         return 'E';
     }
 
--- src/string/string.h
+++ src/string/string.h
@@ -6,6 +6,10 @@
 
 size_t strlen(const char * const str);
 size_t strnlen(const char * const str, size_t max_len);
+
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
+
 bool is_digit(const char c);
 int numeric_char_to_digit(const char c);
 char digit_to_char(const int c);
