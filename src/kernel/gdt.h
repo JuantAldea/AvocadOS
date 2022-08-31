8e15fd49bc0fff8c3fa40aa2142d0c252589d3ae
Juan Antonio <JuantAldea@users.noreply.github.com>
2022-08-31 14:18:45 +0200

    Add defines for constants

--- src/kernel/gdt.h
+++ src/kernel/gdt.h
@@ -5,11 +5,11 @@
 #include "../config.h"
 
 struct gdt_native {
-    uint16_t segment;
+    uint16_t limit_low;
     uint16_t base_addr_0_15_bits;
     uint8_t base_addr_16_23_bits;
     uint8_t access;
-    uint8_t high_low_4_bit_flags;
+    uint8_t limit_high_4_flags_4;
     uint8_t base_addr_24_31_bits;
 } __attribute__((packed));
 
@@ -18,6 +18,11 @@ struct gdt_structure {
     uint32_t limit;
     uint8_t type;
 };
+
+struct gdt_pointer_t {
+	uint16_t limit;
+	uintptr_t base;
+} __attribute__((packed)) ;
 
 struct tss {
     uint32_t link;
@@ -53,5 +58,33 @@ extern struct tss tss;
 extern void tss_load(int tss_segment);
 void gdt_init();
 void tss_set_kernel_stack(uintptr_t kernel_stack_pointer);
+
+
+/// This segment is a data segment
+#define GDT_FLAG_DATASEG 0x02
+/// This segment is a code segment
+#define GDT_FLAG_CODESEG 0x0a
+#define GDT_FLAG_TSS 0x09
+#define GDT_FLAG_TSS_BUSY 0x02
+
+#define GDT_FLAG_SEGMENT 0x10
+/// Privilege level: Ring 0
+#define GDT_FLAG_RING0 0x00
+/// Privilege level: Ring 1
+#define GDT_FLAG_RING1 0x20
+/// Privilege level: Ring 2
+#define GDT_FLAG_RING2 0x40
+/// Privilege level: Ring 3
+#define GDT_FLAG_RING3 0x60
+/// Segment is present
+#define GDT_FLAG_PRESENT 0x80
+/// Segment was accessed
+
+#define GDT_FLAG_ACCESSED 0x01
+#define GDT_FLAG_4K_GRAN 0x80
+#define GDT_FLAG_16_BIT 0x00
+#define GDT_FLAG_32_BIT 0x40
+#define GDT_FLAG_64_BIT 0x20
+
 
 #endif
