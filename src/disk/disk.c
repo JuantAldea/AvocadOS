d25b911d8d9e87a3d2d67c9a0b401947fd15bac6
Juan Antonio <JuantAldea@users.noreply.github.com>
2022-09-04 21:43:09 +0200

    Add ATA ports struct

--- src/disk/disk.c
+++ src/disk/disk.c
@@ -8,52 +8,52 @@
 
 struct disk_t disks[1];
 
+static const struct ata_ports_t ATA_PORTS_PRIMARY = {
+    .data_register = ATA_PRIMARY_IO_BASE_REGISTER,
+    .features_error_register = ATA_PRIMARY_IO_BASE_REGISTER + 1,
+    .sector_count_register = ATA_PRIMARY_IO_BASE_REGISTER + 2,
+    .sector_number_register = ATA_PRIMARY_IO_BASE_REGISTER + 3,
+    .cylinder_low_register = ATA_PRIMARY_IO_BASE_REGISTER + 4,
+    .cylinder_high_register = ATA_PRIMARY_IO_BASE_REGISTER + 5,
+    .drive_head_register = ATA_PRIMARY_IO_BASE_REGISTER + 6,
+    .status_command_register = ATA_PRIMARY_IO_BASE_REGISTER + 7,
+
+    .device_control_alternate_status_register = ATA_PRIMARY_CONTROL_BASE_REGISTER,
+    .drive_address_register = ATA_PRIMARY_CONTROL_BASE_REGISTER + 1,
+};
+
 int disk_read_sector(const struct disk_t *const disk, const int lba, const int n, void *const buf)
 {
     // do not send interrupts. Is a waste of time. We are completely devoted to waste time polling.
-    outb(0x3F6, (uint8_t)2);
-    io_delay();
-
-    outb(0x1F6, (lba >> 24) | disk->port);
-    io_delay();
-    outb(0x1F2, n);
-    io_delay();
-    outb(0x1F3, (uint8_t)(lba & 0xFF));
-    io_delay();
-    outb(0x1F4, (uint8_t)(lba >> 8));
-    io_delay();
-    outb(0x1F5, (uint8_t)(lba >> 16));
-    io_delay();
-    outb(0x1F7, 0x20);
-    io_delay();
-
+    outb(disk->ata_ports->device_control_alternate_status_register, (uint8_t)0x2);
+    outb(disk->ata_ports->drive_head_register, (lba >> 24) | disk->master_slave_flag);
+    outb(disk->ata_ports->sector_count_register, n);
+    outb(disk->ata_ports->sector_number_register, (uint8_t)(lba & 0xFF));
+    outb(disk->ata_ports->cylinder_low_register, (uint8_t)(lba >> 8));
+    outb(disk->ata_ports->cylinder_high_register, (uint8_t)(lba >> 16));
+    outb(disk->ata_ports->status_command_register, ATA_COMMAND_READ_SECTORS);
     uint16_t *ptr = buf;
     uint8_t status;
 
     for (int block = 0; block < n; ++block) {
         do {
-            status = insb(0x1F7);
-            io_delay();
+            status = insb(disk->ata_ports->status_command_register);
         } while ((status & BUSY_BIT) || !(status & DRQ_BIT));
 
         for (int i = 0; i < 256; ++i) {
-            *ptr++ = insw(0x1F0);
-            io_delay();
-            //cache flush
+            *ptr++ = insw(disk->ata_ports->data_register);
             /*
-            outb(0x1F7, 0xE7);
-            io_delay();
+            //cache flush
+            outb(disk->ata_ports->device_control_alternate_status_register, ATA_COMMAND_CACHE_FLUSH);
             do {
-                status = insb(0x1F7);
-                io_delay();
+                status = insb(disk->ata_ports->device_control_alternate_status_register);
             } while (status & BUSY_BIT);
             */
         }
     }
 
     do {
-        status = insb(0x1F7);
-        io_delay();
+        status = insb(disk->ata_ports->device_control_alternate_status_register);
     } while (status & BUSY_BIT);
 
     return 0;
@@ -64,8 +64,10 @@ void disk_init()
     memset(&disks, 0, sizeof(disks)); // NOLINT
     disks[0].type = DISK_TYPE_PHYSICAL;
     disks[0].sector_size = DISK_SECTOR_SIZE;
-    disks[0].port = ATA_MASTER;
+    disks[0].master_slave_flag = ATA_MASTER;
     disks[0].id = 0;
+    disks[0].ata_ports = &ATA_PORTS_PRIMARY;
+    // probing must be the last action
     disks[0].fs_operations = fs_probe_fs(&disks[0]);
 }
 
@@ -75,6 +77,7 @@ int disk_get(int disk_index, struct disk_t **disk)
         *disk = NULL;
         return -ENOMEDIUM;
     }
+
     *disk = &disks[disk_index];
     return 0;
 }
--- src/disk/disk.h
+++ src/disk/disk.h
@@ -12,7 +12,15 @@
 #define DISK_SECTOR_SIZE 512
 #define ATA_MASTER 0xE0
 #define ATA_SLAVE 0xF0
+#define ATA_PRIMARY_DEVICE_CONTROL 0x3F6
+#define ATA_PRIMARY_ALTERNATE_STATUS ATA_PRIMARY_DEVICE_CONTROL
+#define ATA_PRIMARY_STATUS 0x1F7
 
+#define ATA_COMMAND_CACHE_FLUSH ((uint8_t)0xE7)
+#define ATA_COMMAND_READ_SECTORS ((uint8_t)0x20)
+
+#define ATA_PRIMARY_IO_BASE_REGISTER ((uint16_t) 0x1F0)
+#define ATA_PRIMARY_CONTROL_BASE_REGISTER ((uint16_t) 0x3F6)
 // clang-format on
 
 struct filesystem_operations_t;
@@ -20,10 +28,24 @@ struct filesystem_operations_t;
 struct disk_t {
     uint8_t type;
     int sector_size;
-    uint8_t port;
+    uint8_t master_slave_flag;
     int id;
     struct filesystem_operations_t *fs_operations;
-    void* fs_private;
+    void *fs_private;
+    const struct ata_ports_t *ata_ports;
+};
+
+struct ata_ports_t {
+    uint16_t data_register;
+    uint16_t features_error_register;
+    uint16_t sector_count_register;
+    uint16_t sector_number_register;
+    uint16_t cylinder_low_register;
+    uint16_t cylinder_high_register;
+    uint16_t drive_head_register;
+    uint16_t status_command_register;
+    uint16_t device_control_alternate_status_register;
+    uint16_t drive_address_register;
 };
 
 int disk_read_block(const struct disk_t *const disk, const unsigned int lba, const int n, void *const buffer);
